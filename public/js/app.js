724dfafdef3ef3e349e6d5b05cfe5f2eb76e9f9d
Raghvendra Singh <raghvendra.singh@siemens-healthineers.com>
2021-04-19 12:15:43 +0530

    Init Git

---
+++ public/js/app.js
@@ -0,0 +1,36 @@
+console.log('Client side javascript file is loaded!')
+
+fetch('http://puzzle.mead.io/puzzle').then((response) => {
+    response.json().then((data) => {
+        if (data.error) {
+            console.log(data.error)
+        } else {
+            console.log(data)
+            console.log(data.forecast)
+        }
+    })
+})
+
+var fetchDetails=function(value)
+{
+fetch('http://localhost:3000/weather?address='+value).then((response) => {
+    response.json().then((data) => {
+        if (data.error) {
+            console.log(data.error)
+        } else {
+            console.log(data)
+            console.log(data.forecast)
+        }
+    })
+})
+}
+
+var doc=document.querySelector('form');
+
+doc.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const searchVal=document.querySelector('input[type=text]').value;
+    fetchDetails(searchVal);
+    console.log();
+    
+})
